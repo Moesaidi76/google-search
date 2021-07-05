5b6731b49dd9091213bd42e39e6ad2bd30bd98c3
mohamed saidi <mohamedsaidi76@gmail.com>
2021-07-05 17:02:57 -0400

    initial Commit

---
+++ client/build/precache-manifest.b9156c75c8fdae4f0a3899f96164c905.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4794429562c4a4f80841058446778a0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d58dd747c784da556a7",
+    "url": "/static/css/main.8fed18e3.chunk.css"
+  },
+  {
+    "revision": "b1a7531f22060e80a959",
+    "url": "/static/js/2.b315e0a9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b315e0a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d58dd747c784da556a7",
+    "url": "/static/js/main.f58094ae.chunk.js"
+  },
+  {
+    "revision": "9dd6a93b70f2e0c8e403",
+    "url": "/static/js/runtime-main.88672a9d.js"
+  }
+]);
